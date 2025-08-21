4c2dc3cc2ef0a50c88578feb7e3d86d136cfdccb
Andrey <sidr.78@mail.ru>
2025-08-21 16:23:17 +0300

    Environment preparation

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.skillbox"
+version = "0.0.1-SNAPSHOT"
+description = "Final project of the Spring Framework Development course"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/skillbox/hotel_reservations/HotelReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillbox.hotel_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
